8e3f22808dba8bbd34b52c39cf6fce2c31a9a54a
Alina <ms.sciurus@gmail.com>
2025-01-05 13:03:07 +0200

    test1

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import VideoRecorder from './components/VideoRecorder';
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
-
-  const handleVideoRecorded = (videoBlob) => {
-    setVideos([...videos, videoBlob]);
-  };
-
   return (
     <div>
-      <h1>Video Recorder Demo</h1>
-      <VideoRecorder onVideoRecorded={handleVideoRecorded} />
-      <div>
-        <h3>Recorded Videos</h3>
-        {videos.map((video, index) => (
-          <div key={index}>
-            <video
-              src={URL.createObjectURL(video)}
-              controls
-              style={{ width: '100%', marginBottom: '10px' }}
-            ></video>
-          </div>
-        ))}
-      </div>
+      <VideoRecorder />
     </div>
   );
 };
